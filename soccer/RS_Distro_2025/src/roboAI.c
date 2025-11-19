e9de1faf666e8422ff66c4f29e4415b193a8b494
Sebastian Jingoi <sebi.jingoi@gmail.com>
2025-11-18 21:49:57 -0500

    added turn radius function

--- soccer/RS_Distro_2025/src/roboAI.c
+++ soccer/RS_Distro_2025/src/roboAI.c
@@ -625,6 +625,22 @@ void normalize_vector(double *x, double *y) {
     if (mag > 1e-6) { *x /= mag; *y /= mag; }
 }
 
+// dir = 1 => right, dir = 0 => left
+void turn_radius(int pw, double turn_rad, int dir) {
+  double wheel_sep = 0.12;
+  double wheel_turn_rad = turn_rad - (wheel_sep / 2);
+  double p_outer = 1;
+  double p_inner = (turn_rad > 1000) ? 1 : wheel_turn_rad / (wheel_turn_rad + wheel_sep);
+
+  printf("Turning with power po %f pi %f: ", p_outer, p_inner);
+
+  if (dir == 1) {
+    BT_turn(MOTOR_A, pw * p_outer, MOTOR_D, pw * p_inner * 0.95);
+  } else {
+    BT_turn(MOTOR_A, pw * p_inner, MOTOR_D, pw * p_outer * 0.95);
+  }
+}
+
 void move_forward(int pw) {
     BT_drive(MOTOR_A, MOTOR_D, pw);
 }
